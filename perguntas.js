f0b8c7bb564ae56d6f1099a5b2d87fcd1f81c386
muniz006 <00001116970983sp@al.educacao.sp.gov.br>
2024-11-21 08:28:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Meu Malvado Favorito',
+    'Qual o nome das criaturas que ajudam Gru?',
+    'Minions'
+)
+
+criaCartao(
+    'Procurando o Nemo',
+    'Que tipo de peixe é o Nemo?',
+    'Peixe-palhaço'
+)
+
+criaCartao(
+    'Valente',
+    'Qual o animal a mãe de Merida se transforma?',
+    'Urso'
+)
+
+criaCartao(
+    'Rio',
+    'Qual o tipo de ave que Blu representa?',
+    'Ararinha-azul'
+)
+
+criaCartao(
+    'A Princesa e o Sapo',
+    'Em que cidade dos EUA a história se passa?',
+    'Nova Orleans'
+)
+
+criaCartao(
+    'Enrolados',
+    'Qual o nome do camaleão de estimação da Rapunzel?',
+    'Pascal'
+)
+
+criaCartao(
+    'Operação Big Hero?',
+    'Qual a especialidade do robô Baymax?',
+    'Cuidados de saúde'
+)
+
+criaCartao(
+    'Sing',
+    'Qual animal é o personagem Buster Moon?',
+    'Coala'
+)
+
+criaCartao(
+    'O Estranho Mundo de Jack',
+    'Qual o feriado que Jack Skellington tenta assumir?',
+    'Natal'
+)
+
+criaCartao(
+    'Rei Leão',
+    'Quem é o responsável pela morte de Mufasa?',
+    'Scar'
+)
+
+criaCartao(
+    'Kung Fu Panda',
+    'Quais os nomes dos 5 furiosos?',
+    'Garça, Tigresa, Louva-Deus, Macaco e Vibora'
+)
